0bcf7cfc1b4143129094c2615361f23f7bd6a3c3
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:21:34 +0200

    MERGED:improve calculation accuracy
    Allow using float point arguments to avoid truncation.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    
    MERGED:fix truncation error
    
    To convert float to integer the truncation is performed, but the
    rounding is expected.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,30 @@
 #include "calculator.h"
 
-int Calculator::Add (int a, int b)
+int Calculator::Add (double a, double b)
 {
     return a + b;
+<<<<<<< HEAD
+=======
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    return a + b;
+=======
+	return a + b + 0.5;
+>>>>>>> 4ad40a3 (fix truncation error)
+=======
+	return a + b;
+>>>>>>> cbcb06d (formatting: use tabs instead of spaces)
+>>>>>>> 11f2b41 (formatting: use tabs instead of spaces)
+=======
+	return a + b + 0.5;
+>>>>>>> 4ad40a3 (fix truncation error)
+>>>>>>> 35799a3 (fix truncation error)
+>>>>>>> 7aa541c (fix truncation error)
 }
 
-int Calculator::Sub (int a, int b)
+int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
